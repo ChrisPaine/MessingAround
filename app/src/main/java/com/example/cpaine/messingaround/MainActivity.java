4339e91f49f3d4e9ae47105318d924fb62912ee6
Chris Paine <cdpaine2003@gmail.com>
2018-01-14 21:22:56 -0500

    Updated

--- app/src/main/java/com/example/cpaine/messingaround/MainActivity.java
+++ app/src/main/java/com/example/cpaine/messingaround/MainActivity.java
@@ -1,13 +1,31 @@
 package com.example.cpaine.messingaround;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private Button answerYesButton, answerNoButton;
+    private EditText usersNameEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        answerYesButton = (Button)findViewById(R.id.answer_yes_button);
+        answerNoButton = (Button)findViewById(R.id.answer_no_button);
+        usersNameEditText = (EditText)findViewById(R.id.users_name_edit_text);
+    }
+
+    public void onYesButtonClick(View view) {
+        String userName = String.valueOf(usersNameEditText.getText());
+        String yourYesResponse = "That is great " + userName;
+        Toast.makeText(this, yourYesResponse, Toast.LENGTH_SHORT).show();
+    }
+
+    public void onNoButtonClick(View view) {
     }
 }
